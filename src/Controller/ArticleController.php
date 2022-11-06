ea291f9cb6aa6334d8bc5a214d6d56fbffd7ed8d
Alex Otoktone <alexandre.desmot@gmail.com>
2022-11-06 16:37:38 +0100

    front: add view for single article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -15,15 +15,15 @@ class ArticleController extends AbstractController
     #[Route('/articles', name: 'articles_list')]
     public function index(ManagerRegistry $doctrine, mixed $id): Response
     {
-        // Get Article list
+        // Get article repository
         $artileRepository = $doctrine->getRepository(Article::class);
         $articles = $artileRepository->findBy(['id' => $id]);
 
-        // Get Licence list
+        // Get licence repository
         $licenceRepository = $doctrine->getRepository(Licence::class);
         $licences = $licenceRepository->findBy(['id' => $id]);
 
-        // Get Console list
+        // Get console repository
         $consoleRepository = $doctrine->getRepository(Console::class);
         $consoles = $consoleRepository->findBy(['id' => $id]);
 
@@ -32,6 +32,28 @@ class ArticleController extends AbstractController
             'articles' => $articles,
             'licences' => $licences,
             'consoles' => $consoles
+        ]);
+    }
+
+    #[Route('/article/{name}', name: 'show_article')]
+    public function show(ManagerRegistry $doctrine, Article $article, mixed $name): Response
+    {
+        // // Get article repository
+        // $artileRepository = $doctrine->getRepository(Article::class);
+        // $articles = $artileRepository->findBy(['id' => $id]);
+
+        // // Get licence repository
+        // $licenceRepository = $doctrine->getRepository(Licence::class);
+        // $licences = $licenceRepository->findBy(['id' => $id]);
+
+        // // Get console repository
+        // $consoleRepository = $doctrine->getRepository(Console::class);
+        // $consoles = $consoleRepository->findBy(['id' => $id]);
+
+        return $this->render('front/show_article.html.twig', [
+            'article' => $article,
+            // 'console' => $console,
+            // 'licence' => $licence,
         ]);
     }
 }
