38490f17f02b44ee3a769513c3907340bf8f2a7b
Alex Otoktone <alexandre.desmot@gmail.com>
2022-11-06 16:39:16 +0100

    front: update licence controller and views

--- src/Controller/LicenceController.php
+++ src/Controller/LicenceController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use App\Entity\Licence;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,8 +14,9 @@ class LicenceController extends AbstractController
     #[Route('/licences', name: 'licences_list')]
     public function index(ManagerRegistry $doctrine, mixed $id): Response
     {
-        // Get Licence list
+        // Get licence repository
         $licenceRepository = $doctrine->getRepository(Licence::class);
+        // Get licence list
         $licences = $licenceRepository->findAll();
 
         return $this->render('front/licences_list.html.twig', [
@@ -23,17 +25,17 @@ class LicenceController extends AbstractController
         ]);
     }
 
-    #[Route('/licences/{name}', name: 'show_licence')]
-    public function show(ManagerRegistry $doctrine, Licence $licence, mixed $name): Response
+    #[Route('/licences/{id}', name: 'show_licence')]
+    public function show(ManagerRegistry $doctrine, Licence $licence, mixed $id): Response
     {
-        // Get the console name
-        // $console = $doctrine->getRepository(Console::class)->find($name);
-        // Get Articles from the Console name
-        // $articles = $console->getArticles();
+        // Get licence repository
+        $articleRepository = $doctrine->getRepository(Article::class);
+        // Get articles from current licence
+        $articles = $articleRepository->findBy(['licence' => $id]);
 
         return $this->render('front/show_licence.html.twig', [
             'licence' => $licence,
-            // 'articles' => $articles
+            'articles' => $articles
         ]);
     }
 }
