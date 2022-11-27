fecc3e2361e858f3a88b5851cd8b7e065e24980e
Alex Otoktone <alexandre.desmot@gmail.com>
2022-11-27 09:02:42 +0100

    add slider homepage

--- src/Controller/Front/HomeController.php
+++ src/Controller/Front/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Front;
 
+use App\Entity\Article;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function home(): Response
+    public function home(ManagerRegistry $doctrine): Response
     {
+        // get articles repository
+        $articleRepository = $doctrine->getRepository(Article::class);
+        // get list of articles
+        $articles = $articleRepository->findAll();
+
         return $this->render('front/home.html.twig', [
             'controller_name' => 'HomeController',
+            'articles' => $articles,
         ]);
     }
 }
